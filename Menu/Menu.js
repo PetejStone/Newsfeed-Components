abd733f8945a9a4ba1ae76a4bc7673f7ddda4759
PetejStone <petejstoney@gmail.com>
2019-04-10 13:05:54 -0700

    added nav animation

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,9 @@
+const articlesBody = document.querySelector('.articles');
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle('menu--open');
+  menu.classList.toggle('menu--open');//toggles menu open class
+  articlesBody.classList.toggle('article-slide');//toggles article slide class so when menu slides in, so does body content with it
 }
 
 // Start Here: Create a reference to the ".menu" class
